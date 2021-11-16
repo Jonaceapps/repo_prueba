8d5e2cd6303efcb239190af5bc47abc923da6b4b
Jonace <jonathan_miguel_onrubia_tsapp1ta2021@cev.com>
2021-11-16 16:10:09 +0100

    version_inicial

---
+++ database/migrations/2021_11_15_162429_create_personas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 150);
+            $table->string('dni', 10)-> unique();
+            $table->mediumInteger('telefono') -> unsigned() -> unique();
+            $table->text('direccion');
+            $table->string('email', 100)-> nullable()-> unique();
+            $table->string('pass', 200)-> nullable();
+            $table->timestamps(); //fecha de creacion y fecha de ultima edicion
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
